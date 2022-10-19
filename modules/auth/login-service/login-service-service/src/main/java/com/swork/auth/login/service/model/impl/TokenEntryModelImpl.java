b42096f34eb46f605597facefe448567b0cb414e
longnv <longnv@dogoo.vn>
2022-10-19 15:25:35 +0700

    update max length user agent

--- modules/auth/login-service/login-service-service/src/main/java/com/swork/auth/login/service/model/impl/TokenEntryModelImpl.java
+++ modules/auth/login-service/login-service-service/src/main/java/com/swork/auth/login/service/model/impl/TokenEntryModelImpl.java
@@ -97,7 +97,7 @@ public class TokenEntryModelImpl
 	}
 
 	public static final String TABLE_SQL_CREATE =
-		"create table SW_TokenEntry (uuid_ VARCHAR(75) null,tokenId LONG not null primary key,accountId LONG,businessId LONG,userName VARCHAR(75) null,refreshToken VARCHAR(75) null,expiryDate DATE null,issuedDate DATE null,refreshedDate DATE null,userAgent VARCHAR(75) null,clientIp VARCHAR(75) null,firstLoginDate DATE null,sessionId VARCHAR(75) null,remember BOOLEAN)";
+		"create table SW_TokenEntry (uuid_ VARCHAR(75) null,tokenId LONG not null primary key,accountId LONG,businessId LONG,userName VARCHAR(75) null,refreshToken VARCHAR(75) null,expiryDate DATE null,issuedDate DATE null,refreshedDate DATE null,userAgent VARCHAR(200) null,clientIp VARCHAR(75) null,firstLoginDate DATE null,sessionId VARCHAR(75) null,remember BOOLEAN)";
 
 	public static final String TABLE_SQL_DROP = "drop table SW_TokenEntry";
 
